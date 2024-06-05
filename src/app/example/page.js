1b0eff82582c4ddc70f472696dbce5d70d59d125
Chelsea <nnajioke0708@gmail.com>
2024-06-05 06:14:26 -0700

    Nextjs App

---
+++ src/app/example/page.js
@@ -0,0 +1,27 @@
+//css in js
+// const ExamplePage = () => {
+
+//     const abc = {
+//         display: 'flex',
+//         padding:'50px',
+//     }
+//     return (
+//         <div style={abc}>
+//         <div>1</div>
+//         <div>3</div> 
+//         </div>  
+//     )
+// };
+
+// export default ExamplePage;
+
+const AboutPage = () => {
+
+    return (
+        <div>
+            <h1>jhdhbdbhbdbhjbjhds</h1>
+        </div>
+    )
+}
+
+export default AboutPage;
